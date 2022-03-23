b4b8a9570151c67b6499d3c5ed3a43e00758d69b
Sergio Pugliese <pugliese.sergio1987@hotmail.it>
2022-03-23 09:19:59 +0100

    angular

---
+++ angular-tour-of-hero/src/app/components/hero-datail/hero-datail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-datail',
+  templateUrl: './hero-datail.component.html',
+  styleUrls: ['./hero-datail.component.scss']
+})
+export class HeroDatailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
